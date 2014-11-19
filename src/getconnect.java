217a52c2e5bd39583d543c55421d15324c55e110
tohai <tohaia2k47@gmail.com>
2014-11-20 01:53:22 +0700

    getconvamainframe

---
+++ src/getconnect.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+public class getconnect {
+private Connection a;
+public void setconnect(Connection a)
+{
+this.a=a;
+}
+public Connection getcon(){
+return this.a;
+}
+public getconnect()
+{
+Connection a1;
+try
+{
+Class.forName("org.postgresql.Driver");
+a1 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/kitucxa",
+"postgres", "123456");
+setconnect(a1);
+}
+catch (Exception ex)
+{
+JOptionPane.showMessageDialog(null, "so ban qua den");
+System.out.println(ex);
+}
+}
+}
